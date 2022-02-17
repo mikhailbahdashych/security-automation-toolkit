6ff6e56ba232c01f6254ccf9bd95ff5b0a805c8d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-17 18:42:58 +0100

    regex email

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
+const asyncMiddleware = require('./middlewares/async')
 const accountController = require('./controllers/accountController')
 
-router.post('/login', accountController.login)
-router.post('/register', accountController.register)
+router.post('/login', asyncMiddleware(accountController.login))
+router.post('/register', asyncMiddleware(accountController.register))
 
 export default router;
