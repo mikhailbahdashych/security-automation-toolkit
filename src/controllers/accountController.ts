10f56a701725d4d390bcab864b3d1301a7c1d128
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-20 10:21:22 +0100

    logger fixes

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -4,18 +4,22 @@ import loggerConfig from '../common/logger'
 const accountService = require('../services/accountService')
 const jwtService = require('../services/jwtService')
 const cryptoService = require('../services/cryptoService')
-const logger = loggerConfig({ label: 'balances-controller', path: 'balances' })
+const logger = loggerConfig({ label: 'account-controller', path: 'account' })
 
 export const register = async (req: Request, res: Response) => {
   try {
     let { email, password } = req.body
     const user = await accountService.getUserByEmail(email)
-    logger.info(`Registration user with email ${email}`)
+    logger.info(`Registration user with email: ${email}`)
 
+    // @TODO DO SOMETHING WITH STATUSES
     if (!user) {
       password = cryptoService.hashPassword(password, process.env.CRYPTO_SALT)
       await accountService.createUser({ email, password })
       res.status(200).json({ status: 1 })
+    } else {
+      logger.info(`User with email ${email} already exists`)
+      res.status(500).json({message: 'Something went wrong'})
     }
 
   } catch (e) {
