479019ff6636f27250f3bccb3813d27f14eff043
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-23 21:59:20 +0100

    ts fixes undefiend

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -7,41 +7,47 @@ import { JwtPayload } from "../interfaces/jwt";
 const privateKey = fs.readFileSync(path.resolve(__dirname + "../../../keys/private.pem"));
 const publicKey = fs.readFileSync(path.resolve(__dirname + "../../../keys/public.pem"));
 
-export = {
-  sign: (payload: JwtPayload) => {
-    try {
-      return jwt.sign(
-        payload,
-        {
-          key: privateKey,
-          passphrase: process.env.JWT_PASSPHRASE.toString()
-        },
-        {
-          algorithm: "RS256",
-          expiresIn: "30m"
-        }
-      )
-    } catch (e) {
-      //
-    }
-  },
-  getUserPromise: (token: string) => {
-    try {
-      return new Promise(((resolve, reject) => {
-        jwt.verify(token, publicKey, (err: any, decoded: any) => {
-          if (!err) {
-            return resolve(decoded);
-          } else {
-            return reject(err);
-          }
-        })
-      }))
-    } catch (e) {
-      //
-    }
-  },
-  getUser: async (token: string) => {
+export const sign = (payload: JwtPayload) => {
+  try {
+    return jwt.sign(
+      payload,
+      {
+        key: privateKey,
+        passphrase: process.env.JWT_PASSPHRASE.toString()
+      },
+      {
+        algorithm: "RS256",
+        expiresIn: "30m"
+      }
+    )
+  } catch (e) {
+    //
+  }
+}
+
+
+export const getUser = async (token: string) => {
+  try {
     // @ts-ignore
     return await this.getUserPromise(token)
+  } catch (e) {
+    //
+  }
+}
+
+
+export const getUserPromise = (token: string) => {
+  try {
+    return new Promise(((resolve, reject) => {
+      jwt.verify(token, publicKey, (err: any, decoded: any) => {
+        if (!err) {
+          return resolve(decoded);
+        } else {
+          return reject(err);
+        }
+      })
+    }))
+  } catch (e) {
+    //
   }
 }
