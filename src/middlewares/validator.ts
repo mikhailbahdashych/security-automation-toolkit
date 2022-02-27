577a87b33d44839f16c3921fc292f5108969f5cb
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-27 22:29:09 +0100

    email validator

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -1,26 +1,15 @@
-import * as validator from 'express-validator'
+import { validationResult } from 'express-validator'
+import { Request, Response } from "express";
 
 export default (fields: any) => {
-  return async (req: any, res: any, next: any) => {
+  return async (req: Request, res: Response, next: any) => {
     for (const field of fields) {
       await require(`./validators/${field}`)(req);
     }
-    const errors = validator.validationResult(req);
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     next();
   }
 }
-// exports.reqValidator = (fields) => {
-//   return async (req, res, next) => {
-//     for (const field of fields) {
-//       await require(`./validators/${field}`)(req);
-//     }
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-//     next();
-//   };
-// };
