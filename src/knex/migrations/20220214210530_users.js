be073ef593eace678fc802d7de3401964181cc2f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-16 21:46:56 +0100

    migrations fixes

--- src/knex/migrations/20220214210530_users.js
+++ src/knex/migrations/20220214210530_users.js
@@ -1,8 +1,14 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
     table.uuid('id').notNullable().defaultTo(knex.raw('gen_random_uuid ()')).primary()
+    table.text('personaluuid')
     table.text('email').notNullable()
+    table.boolean('confirmemail').notNullable().defaultTo(false)
+    table.boolean('emailchange').notNullable().defaultTo(false)
     table.text('password').notNullable()
+    table.text('phone').nullable()
+    table.text('phonecode').nullable()
+    table.text('twofa').nullable()
 
     table.timestamp("createdat").defaultTo(knex.fn.now())
     table.timestamp("updatedat").defaultTo(knex.fn.now())
