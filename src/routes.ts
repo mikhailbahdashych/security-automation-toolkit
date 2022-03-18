e3b76b823d1cd31b0cb6dc9aae035e3af67a3d87
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-18 17:09:37 +0100

    nah

--- src/routes.ts
+++ src/routes.ts
@@ -6,26 +6,25 @@ const reflinkController = require('./controllers/reflinkController')
 
 // @TODO validator(['email', 'phone']) Something with validators
 import validator from "./middlewares/validator";
-import auth from "./middlewares/auth";
 
 // Basic functions
 router.post('/login', accountController.login)
 router.post('/register', accountController.register)
 router.post('/confirm-registration', accountController.confirmRegistration)
-router.post('/close-account', auth, accountController.closeAccount)
-router.post('/freeze-account', auth, accountController.freezeAccount)
+router.post('/close-account', accountController.closeAccount)
+router.post('/freeze-account', accountController.freezeAccount)
 
 // 2FA
-router.post('/set-2fa', auth, accountController.set2fa)
-router.post('/disable-2fa', auth, accountController.disable2fa)
-router.post('/verify-2fa', auth, accountController.verify2fa)
+router.post('/set-2fa', accountController.set2fa)
+router.post('/disable-2fa', accountController.disable2fa)
+router.post('/verify-2fa', accountController.verify2fa)
 
 // Password
 router.post('/reset-password', accountController.resetPassword)
-router.post('/change-password', auth, accountController.changePassword)
+router.post('/change-password', accountController.changePassword)
 
 // Email
-router.post('/change-email', auth, accountController.changeEmail)
+router.post('/change-email', accountController.changeEmail)
 router.post('/send-email', emailController.sendEmail) // ?
 
 // Token
@@ -35,8 +34,8 @@ router.post('/client-by-token', accountController.clientByToken) // ?
 router.post('/verification-code', accountController.sendVerificationCode) // ?
 
 // Referral links
-router.post('/generate-referral-link', auth, reflinkController.generateReferralLink)
-router.post('/get-referral-link', auth, reflinkController.getReferralLink)
-router.get('/get-clients-by-referral-link/:reflink', auth, reflinkController.getClientsByReferralLink)
+router.post('/generate-referral-link', reflinkController.generateReferralLink)
+router.post('/get-referral-link', reflinkController.getReferralLink)
+router.get('/get-clients-by-referral-link/:reflink', reflinkController.getClientsByReferralLink)
 
 export default router;
