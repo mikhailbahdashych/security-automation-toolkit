0742ae692c686fa2c34f1adf132c7ee000660abf
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-24 22:00:21 +0100

    getting client wallets

--- src/controllers/walletController.ts
+++ src/controllers/walletController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import loggerConfig from '../common/logger'
 
 import * as walletService from '../services/walletService';
+import { getClientByJwtToken } from "../common/getClientByJwtToken";
 import { CommonResponse } from "../responses/response";
 
 const logger = loggerConfig({ label: 'wallet-controller', path: 'wallet' })
@@ -9,6 +10,11 @@ const logger = loggerConfig({ label: 'wallet-controller', path: 'wallet' })
 export const checkWallets = async (req: Request, res: Response) => {
   try {
     const { token } = req.body
+    const client = await getClientByJwtToken(token)
+    if (!client) return CommonResponse.common.accessForbidden({ res })
+
+    const clientWallets = await walletService.getWalletsByClientId(client.id)
+
   } catch (e) {
     logger.error(`Error while checking wallets => ${e}`)
     return CommonResponse.common.somethingWentWrong({ res })
--- src/services/walletService.ts
+++ src/services/walletService.ts
@@ -2,5 +2,7 @@ const knex = require('../knex/knex.js')
 const tableName = 'balances'
 
 export const getWalletsByClientId = async (id: string) => {
-
+  return knex(tableName)
+    .select('*')
+    .where('clientid', id)
 }
