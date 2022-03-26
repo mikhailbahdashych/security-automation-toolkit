6ebe1c6f2f56f26cea470aec015f4720a953107d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-26 22:45:19 +0100

    new migration

--- src/knex/migrations/20220326214257_create_table_frezed_accounts.js
+++ src/knex/migrations/20220326214257_create_table_frezed_accounts.js
@@ -1,15 +1,18 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('freezedaccounts', table => {
+    table.uuid('id').notNullable().defaultTo(knex.raw('gen_random_uuid ()')).primary()
+
+    table.uuid('clientid').notNullable()
+    table
+      .foreign('clientid')
+      .references('id')
+      .inTable('clients')
+
+    table.timestamp("createdat").defaultTo(knex.fn.now())
+    table.timestamp("updatedat").defaultTo(knex.fn.now())
+  })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('freezedaccounts')
 };
